a776b4e1e8c69daa539eda1f3cdefd39f7407f17
JoelShepard <57812697+JoelShepard@users.noreply.github.com>
2024-09-30 23:01:11 +0200

    update - esercizi - gitignore

---
+++ Laboratorio/1/Esercizio 5/max_min_med.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+
+    int a, b, c, d, e, f;
+
+    printf("Scegli valori interi positivi: ");
+    scanf("%d", &a);
+    scanf("%d", &b);
+    scanf("%d", &c);
+    scanf("%d", &d);
+    scanf("%d", &e);
+    scanf("%d", &f);
+
+
+    if(a <= 0 || b <= 0 || c <= 0 || d <= 0 || e <= 0 || f <= 0) {
+        printf("\nI valori inseriti devono essere degli interi positivi");
+        }
+
+    
+
+}
