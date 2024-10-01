8eda66cfcd507cb25dbb6bab12445cc7cf4a1a74
JoelShepard <57812697+JoelShepard@users.noreply.github.com>
2024-10-01 18:22:13 +0200

    Esercizio 3 finito

--- Laboratorio/1/Esercizio 3/somme_successive.c
+++ Laboratorio/1/Esercizio 3/somme_successive.c
@@ -4,18 +4,25 @@ int main(){
     int a, b, c;
     a = 0;
     b = 0;
-    c = 1;
+    c = 0;
     printf("inserisci il primo fattore:");
     scanf("%d", &a);
     printf("inserisci il secondo fattore:");
     scanf("%d", &b);
 
-    while(b >= 0){
-        c = a + c;
+    if (b > 0) {
+        while(b > 0){
+        c = c + a;
         b = b - 1;
+        }   
     }
-    
+    if (b < 0) {
+        while(b < 0){
+        c = c + a;
+        b = b + 1;
+        }
+        c = - c;   
+    }      
+
     printf("il risultato è: %d", c);
 }
-
-// da finire
