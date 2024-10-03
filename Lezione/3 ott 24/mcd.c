235f455a5a41507f0b4c0111044faad552b7e4b0
Rag3005 <gioelerosana@outlook.it>
2024-10-03 08:23:54 +0000

    update

---
+++ Lezione/3 ott 24/mcd.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(){
+    int a,b;
+    int mcd =1;
+    int min;
+
+    printf("inserisci un valore: ");
+    scanf("%d", &a);
+    printf("inserisci un altro valore: ");
+    scanf("%d", &b);
+
+    if (a < b){
+        min = a;
+    } else {
+        min = b;
+    }
+
+    
+}
