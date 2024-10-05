7c084e6d5fde985a46ce8e46e20af69566fdb4a5
JoelShepard <57812697+JoelShepard@users.noreply.github.com>
2024-10-05 22:54:59 +0200

    completati esercizi fino al 6 - manca 7 + quelli assegnati a lezione

--- Laboratorio/1/Esercizio 6/sommatoria.c
+++ Laboratorio/1/Esercizio 6/sommatoria.c
@@ -1,15 +1,28 @@
 #include <stdio.h>
 
 int main(){
-    int r, q, y;
+    int r, q, iter; 
+    float tot = 0.0;
 
-    printf("\n inserisci il primo estremo della somma:");
+    printf("\nInserisci il primo estremo della sommatoria:");
     scanf("%d", &q);
-    printf("\n inserisci il secondo estremo della somma:");
-    scanf("%d\n", &r);
+    printf("Inserisci il secondo estremo della sommatoria:");
+    scanf("%d", &r);
 
     if(q < 0 || r < 0){
-        printf("\nEstremi inseriti non validi. Inserisci interi positivi diversi da 0");
+        printf("\nEstremi inseriti non validi. Inserisci interi positivi diversi da 0\n");
+        return 0;
+    } else if(r < q){
+        printf("\nIl secondo estremo deve essere più grande del primo\n");
         return 0;
     }
+
+    iter = r - q;
+
+    while(iter >= 0){
+        tot = tot + (1.0 / q);
+        iter--;
+        q++;
+    }
+    printf("\nIl risultato della sommatoria è: %f\n", tot);
 }
--- Lezione/3 ott 24/mcd_gpt.c
+++ Lezione/3 ott 24/mcd_gpt.c
@@ -3,23 +3,23 @@
 int main() {
     int num1, num2, mcd = 1, i = 1;
 
-        // Chiedere all'utente di inserire due numeri
-            printf("Inserire il primo numero: ");
-                scanf("%d", &num1);
-                    printf("Inserire il secondo numero: ");
-                        scanf("%d", &num2);
+    // Chiedere all'utente di inserire due numeri
+    printf("Inserire il primo numero: ");
+    scanf("%d", &num1);
+    printf("Inserire il secondo numero: ");
+    scanf("%d", &num2);
 
-                            // Ricerca del massimo comune divisore 
-                                // senza usare cicli for
-                                    while (i <= num1 && i <= num2) {
-                                            if (num1 % i == 0 && num2 % i == 0) {
-                                                        mcd = i; // Aggiorna il MCD se i è un divisore comune
-                                                                }
-                                                                        i++; // Incrementa i
-                                                                            }
+    // Ricerca del massimo comune divisore 
+    // senza usare cicli for
+    while (i <= num1 && i <= num2) {
+        if (num1 % i == 0 && num2 % i == 0) {
+            mcd = i; // Aggiorna il MCD se i è un divisore comune
+            }
+        i++; // Incrementa i
+        }
 
-                                                                                // Visualizzare il risultato
-                                                                                    printf("Il massimo comune divisore è: %d\n", mcd);
+    // Visualizzare il risultato
+    printf("Il massimo comune divisore è: %d\n", mcd);
 
-                                                                                        return 0;
-                                                                                        }
+    return 0;
+}
