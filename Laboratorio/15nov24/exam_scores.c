1d786a9dc56da947f81e15d19855806e756ea683
JoelShepard <57812697+JoelShepard@users.noreply.github.com>
2024-11-24 18:14:20 +0100

    update 24nov 2024

---
+++ Laboratorio/15nov24/exam_scores.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <string.h>
+
+#define NLEN 14
+#define N 3
+
+typedef struct {
+    char name[NLEN];
+    int code;
+    int score;
+} exam;
+
+int main(){
+    FILE * mydb;
+    int i=0, lastchar=0;
+    exam allexams[N];
+
+    printf("Benvenuto!\n");
+
+    mydb = fopen("esiti.bin", "w");
+    if(mydb == NULL){
+        printf("Il file non è stato aperto correttamente."); return 0;
+    }
+
+    while (i<N){
+        //name input
+        printf("Inserisci il cognome del candidato %d: ", i+1);
+        fgets(allexams[i].name, NLEN, stdin);
+        lastchar = strlen(allexams[i].name);
+        allexams[i].name[lastchar - 1] = '\0';
+        fprintf(mydb, "%s,", allexams[i].name);
+
+        //code input
+        printf("Inserisci il numero di matricola del candidato %d: ", i+1);
+        scanf("%d", &allexams[i].code);
+        fprintf(mydb, "%d,", allexams[i].code);
+
+        //score input
+        printf("Inserisci il voto del candidato %d: ", i+1);
+        scanf("%d", &allexams[i].score);
+        fprintf(mydb, "%d,", allexams[i].score);
+
+        //newline + increase
+        fprintf(mydb, "\n");
+        i++;
+    }
+    fclose(mydb);
+    return 0;
+}
