452fe2151c8774816757a1d01c08cc476acf5390
JoelShepard <57812697+JoelShepard@users.noreply.github.com>
2024-11-30 15:29:32 +0100

    esercizio 3 - Lab 29nov24

--- Laboratorio/29nov24/es3/custom_lib.h
+++ Laboratorio/29nov24/es3/custom_lib.h
@@ -1,10 +1,10 @@
-#ifndef __MYLIB__
-#define __MYLIB__
+#ifndef _CUSTOM_LIB_H_
+#define _CUSTOM_LIB_H_
 
 char* invertiStringa(char *str);
-//void toUppercase(char *str);
-//void rimuoviSpazi(char *str);
-//int isPalindromo(char *str);
-//int contaVocali(char *str);
+char* toUppercase(char *str);
+char* rimuoviSpazi(char *str);
+int isPalindromo(char *str);
+int contaVocali(char *str);
 
 #endif
--- Laboratorio/29nov24/es3/custom_lib_program.c
+++ Laboratorio/29nov24/es3/custom_lib_program.c
@@ -1,8 +1,30 @@
 #include "custom_lib.h"
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(){
-    char mystring[]="Ciao a tutti e otto";
+    char mystring[]="otto otto";
 
-    printf("%s", (char*)invertiStringa(mystring));
+    if (invertiStringa(mystring)!=NULL){
+        printf("Stringa invertita: %s\n", invertiStringa(mystring));
+        free(invertiStringa(mystring));
+    }
+
+    if (toUppercase(mystring)!=NULL){
+        printf("Stringa maiuscola: %s\n", toUppercase(mystring));
+        free(toUppercase(mystring));
+    }
+
+    if (rimuoviSpazi(mystring)!=NULL){
+        printf("Stringa senza spazi: %s\n", rimuoviSpazi(mystring));
+        free(rimuoviSpazi(mystring));
+    }
+
+    if (isPalindromo(mystring)==1) {
+        printf("La stringa è palindroma\n");
+    } else if (isPalindromo(mystring)==0) {
+        printf("La stringa non è palindroma\n");
+    }
+
+    printf("Il numero di vocali contenute è: %d\n", contaVocali(mystring));
 }
