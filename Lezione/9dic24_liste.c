4ee7d1169e15a9749341ea1643825425167149c3
iPad <gioelegr5@icloud.com>
2024-12-09 10:32:19 +0100

    mid update

--- Lezione/9dic24_liste.c
+++ Lezione/9dic24_liste.c
@@ -13,6 +13,8 @@ int list_void_check(List l);
 int list_dim(List l);
 int rec_list_dim(Lit l);
 void print_list(List l);
+void print_list_reverse(List l);
+int elem_data_check(List l, int val);
 
 List int_list(){
     return NULL;
@@ -41,9 +43,29 @@ int rec_list_dim(List l){
 
 void print_list(List l){
     if (l==NULL) printf("-|");
-    printf("%d->", (*l).data);
-    print_list((*l).next);
+    while(l!=NULL){
+		printf("%d->", (*l).data);
+		print_list((*l).next);
+    }
 }
+
+void print_list_reverse(List l){
+    if (l==NULL) printf("|-");
+	while(l!=NULL){
+		print_list((*l).next);
+    	printf("%d->", (*l).data);
+    }
+}
+
+int elem_data_check(List l, int val){
+    ElemList* cur = l;
+    while(cur!=NULL && (*cur).data != val){
+		cur = (*cur).next;
+    }
+    if((*cur).data == val) return 1;
+    return 0;
+}
+
 
 int main(){
     List L;
