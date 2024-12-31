fae41234a5a4c3143a5f0d2003b04db1c6a347d5
JoelShepard <57812697+JoelShepard@users.noreply.github.com>
2024-12-31 15:55:29 +0100

    Esercizio Q2 20-01-24. Corretto per la maggior parte. Controlla le Note per saperne di più

--- Temi d'esame/20-01-2024/Q2.c
+++ Temi d'esame/20-01-2024/Q2.c
@@ -10,42 +10,47 @@ typedef struct {
 void Ripulisci(FILE *fin, FILE *fout){
     elem list[MAX_PROD];
     char c;
-    int count = 0;
+    int count = 1;
 
     while((c = fgetc(fin)) != EOF){
         if(c == '\n'){
             count++;
         }
     }
-    
+
     //insert
+    rewind(fin);
     for(int i=0; i<count; i++){
-        fscanf(fin, "%s %d", list[i].name, &list[i].qta);
+        fscanf(fin, "%s %d\n", list[i].name, &(list[i].qta));
     }
 
     //clean up
     int i=0, j=0;
     for(i=0; i<count; i++){
         int ptot=0;
+        if (list[i].qta == -1) {
+            continue;
+        }
         for(j=i+1; j<count; j++){
             if(strcmp(list[j].name, list[i].name)==0){
                 ptot = ptot + list[j].qta;
+                list[j].qta = -1;
             }
         }
-        fprintf(fout,"%s %d", list[i].name, list[i].qta+ptot);
+        fprintf(fout,"%s %d\n", list[i].name, list[i].qta+ptot);
     }
 }
 
 int main() {
     FILE *fin, *fout;
-    
+
     // Apertura file di input
     fin = fopen("prodotti.txt", "r");
     if (fin == NULL) {
         printf("Errore nell'apertura del file di input\n");
         return 1;
     }
-    
+
     // Apertura file di output
     fout = fopen("risultati.txt", "w");
     if (fout == NULL) {
@@ -53,14 +58,14 @@ int main() {
         fclose(fin);
         return 1;
     }
-    
+
     // Chiamata alla funzione Ripulisci
     Ripulisci(fin, fout);
-    
+
     // Chiusura dei file
     fclose(fin);
     fclose(fout);
-    
+
     printf("Elaborazione completata con successo!\n");
     return 0;
 }
