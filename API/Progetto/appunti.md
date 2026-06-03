1e963fa46469ad341c0ccc027864b774e603259b
JoeShepard <gioelerosana@outlook.it>
2026-06-03 11:46:04 +0200

    Update

---
+++ API/Progetto/appunti.md
@@ -0,0 +1,4 @@
+# Appunti
+Usa il comando `man` per navigare la standard library del C e usa il più possibile quelle funzioni
+
+Ad esempio strtok divide una stringa per spazi (comoda)
--- API/Sorting/countingsort.c
+++ API/Sorting/countingsort.c
@@ -15,29 +15,36 @@ void CountingSort(int* array){
     int output[ELEMENTS];
     int max = array[0];
 
+    /*Cerca il massimo */
     for (int i=1; i<ELEMENTS; i++){
         if (array[i] > max)
             max = array[i];
     }
 
     int* count = calloc(max+1, sizeof(int));
-    /*In questo caso, dato che sappiamo che i numeri generati saranno da 0 a 999
-    potremmo allocare questo array staticamente */
+    /* Dato che sappiamo che i numeri generati saranno da 0 a 999
+    potremmo allocare array count staticamente */
     if (count == NULL) return;
 
+    /* Conta le occorrenze di ciascun valore nell'array */
     for (int i=0; i<ELEMENTS; i++){
         count[array[i]]++;
     }
 
+    /* Calcola le posizioni cumulative per determinare
+    l'indice finale di ciascun elemento nell'array ordinato */
     for (int i=1; i<=max; i++){
         count[i] += count[i-1];
     }
 
+    /* Posiziona ogni elemento nell'array di output
+    scorrendo da destra verso sinistra per mantenere la stabilita' */
     for (int i=ELEMENTS-1; i>=0; i--){
         output[count[array[i]] - 1] = array[i];
         count[array[i]]--;
     }
 
+    /* Copia l'array ordinato nell'array originale */
     for (int i=0; i<ELEMENTS; i++){
         array[i] = output[i];
     }
